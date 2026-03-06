5738c63573d7c6b1c0759baad244c2b48bcc0786
git push <raulmahya11@gmail.com>
2026-03-06 09:04:53 +0700

    finis

--- database/seeders/FormsSeeder.php
+++ database/seeders/FormsSeeder.php
@@ -10,6 +10,8 @@ class FormsSeeder extends Seeder
     public function run(): void
     {
         DB::table('forms')->insert([
+            'company_id' => 2,
+            'site_id' => 5,
             'department_id' => 5,
             'created_by' => 1,
             'title' => 'SHE SOP 030 Pengelolaan Keselamatan',
@@ -19,7 +21,7 @@ class FormsSeeder extends Seeder
             'type' => 'pdf',
             'schema' => null,
             'pdf_path' => 'assets/form/SHE-SOP--030-Pengelolaan-Keselamatan.pdf',
-            'is_active' => 1,
+            'is_active' => true,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
